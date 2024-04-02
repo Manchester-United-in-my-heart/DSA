701db62035df7d07070baffde02227f6bbe28cd1
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-04-02 16:34:20 +0700

    stack: init, mt, pop, push, size, top

---
+++ stl/stack.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int> the_stack = stack<int>({1, 2, 3});
+    // 1. empty()
+    // true if stack is empty
+    // 2. pop()
+    // remove the top element
+    // 3. push()
+    // push an element to top
+    // 4. size()
+    // get the size of stack
+    // 5. top()
+    // return the top element's pointer
+    cout << the_stack.top();
+    return 0;
+}
