f644a7af35a9c777c6a402f532a75ce581751338
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-04-05 10:31:40 +0700

    singly linked list: insert, delete

--- DS/single_link_list.cpp
+++ DS/single_link_list.cpp
@@ -3,101 +3,77 @@ using namespace std;
 
 class Node
 {
-    int val;
-    Node *next_node;
-
 public:
+    int data;
+    Node *next;
     Node()
     {
-        val = 0;
-        next_node = NULL;
-    }
-    Node(int new_val, Node *nextnode)
-    {
-        val = new_val;
-        next_node = nextnode;
-    }
-    void set_value(int new_val)
-    {
-        val = new_val;
-    }
-    int get_value()
-    {
-        return this->val;
+        data = 0;
+        next = NULL;
     }
 
-    void set_next_node(Node *node)
+    Node(int data)
     {
-        next_node = node;
-    }
-
-    Node *get_next_node()
-    {
-        return next_node;
+        this->data = data;
+        this->next = NULL;
     }
 };
 
 class LinkedList
 {
-
     Node *head;
 
 public:
-    Node *get_head()
-    {
-        return head;
-    };
-    void set_head(Node *new_head)
-    {
-        head = new_head;
-    }
-    LinkedList(int new_val)
-    {
-        head = new Node(new_val, NULL);
-    };
-    void push_back(int new_val)
-    {
-        Node *search = this->get_head();
-        while ( (*search).get_next_node() != NULL ) {
-            search = (*search).get_next_node();
-        }
-        Node new_node = Node(new_val, NULL);
-        search->set_next_node(&new_node);
-    }
-    void insert(Node *nextnode, int new_val)
-    {
-        if (nextnode == head)
-        {
-            Node new_node = Node(new_val, head);
-            this->set_head(&new_node);
-        }
-        else
-        {
-            Node search = *this->get_head();
-            while (&search != NULL && search.get_next_node() != nextnode)
-            {
-                search = *search.get_next_node();
-            }
-            if (&search == NULL)
-            {
-                cout << "Exit without changed";
-            }
-            else
-            {
-                Node new_node = Node(new_val, search.get_next_node());
-                search.set_next_node(&new_node);
-                cout << "Changed";
-            }
-        }
-    }
+    LinkedList() { head = NULL; }
+    void insertNode(int);
+    void printList();
+    void deleteNode(int);
 };
+
+void LinkedList::insertNode(int data)
+{
+    Node *new_node = new Node(data);
+
+    while (head == NULL)
+    {
+        head = new_node;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+}
+
+void LinkedList::deleteNode(int data)
+{
+    Node *temp = this->head;
+
+    if ((*temp).data == data)
+    {
+        *temp = *(temp->next);
+        return;
+    }
+
+    while (temp->next != NULL && (*(*temp).next).data != data)
+    {
+        temp = temp->next;
+    }
+
+    if (temp->next == NULL)
+        return;
+    (*temp).next = (*((*temp).next)).next;
+}
 
 int main()
 {
-    LinkedList list1 = LinkedList(10);
-    list1.push_back(20);
-    list1.push_back(30);
-    Node *head = (*list1.get_head()).get_next_node();
-    list1.insert(head, 15);
+    LinkedList ll = LinkedList();
+    ll.insertNode(10);
+    ll.insertNode(20);
+    ll.insertNode(30);
+    ll.deleteNode(50);
     return 0;
 }
